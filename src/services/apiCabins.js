29f672e31b10a08c5a8776545f5a8a9fe9e91710
AyaHusein2001 <aya.ahmed2001.aa@gmail.com>
2025-11-20 16:34:29 +0200

    added react query , delete , add cabin , added react hook form too

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -7,5 +7,41 @@ export async function getCabins() {
     console.error(error);
     throw new Error("Cabins could not be loaded");
   }
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleted");
+  }
+  return data;
+}
+
+export async function createCabin(cabin) {
+  const imageName = `${Math.random()}-${cabin.image.name}`.replaceAll("/", "");
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+  //1. Create the cabin
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([{ ...cabin, image: imagePath }]) // it will work as the form fields names are exactly the same as the fields in the table
+    .select();
+
+  //2. Upload the cabin image
+  const { storageError } = await supabase.storage
+    .from("cabin-images") //storage name
+    .upload(imageName, cabin.image); //first is the img name , 2nd is the file
+
+  //3.delete the cabin if there was an error creating the corresponding image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.error(error);
+    throw new Error(
+      "Cabin Image could not be uploaded and the cabin could not be created"
+    );
+  }
+
   return data;
 }
